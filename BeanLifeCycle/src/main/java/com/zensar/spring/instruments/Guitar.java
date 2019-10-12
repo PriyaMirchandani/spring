0da6c22664ec751b9b6a75c51bc678f477e3d919
PriyaMirchandani <55914068+PriyaMirchandani@users.noreply.github.com>
2019-10-11 21:26:42 -0700

    Add files via upload

---
+++ BeanLifeCycle/src/main/java/com/zensar/spring/instruments/Guitar.java
@@ -0,0 +1,23 @@
+package com.zensar.spring.instruments;
+
+public class Guitar implements Instrument {
+
+	@Override
+	public void play() {
+		System.out.println("ting ting ting");
+	}
+
+	@Override
+	public void tune() {
+		// TODO Auto-generated method stub
+		System.out.println("guitar is tuned");
+	}
+
+	@Override
+	public void clean() {
+		// TODO Auto-generated method stub
+		System.out.println("guitar is clean");
+		
+	}
+
+}
---
+++ ListOrSetInjection/src/main/java/com/zensar/spring/performers/Instrumentalist.java
@@ -0,0 +1,55 @@
+package com.zensar.spring.performers;
+
+import java.util.List;
+import java.util.Set;
+
+import com.zensar.spring.instruments.Instrument;
+
+public class Instrumentalist implements Performer {
+
+
+	private String song;
+	private Set<Instrument> instruments;
+	
+	public Instrumentalist() {
+		
+	System.out.println("instrumentalist is created");
+	}
+	
+	
+
+	public Set<Instrument> getInstruments() {
+		return instruments;
+	}
+
+
+
+	public void setInstruments(Set<Instrument> instruments) {
+		this.instruments = instruments;
+	}
+
+
+
+	public String getSong() {
+		return song;
+	}
+
+
+	public void setSong(String song) {
+		this.song = song;
+		System.out.println("Song is set");
+	}
+
+
+	@Override
+	public void perform() {
+		System.out.println("instrumentalist is playing a song "+ song);
+		
+		for (Instrument instrument : instruments) {
+			
+			instrument.play();
+		}
+
+	}
+
+}
